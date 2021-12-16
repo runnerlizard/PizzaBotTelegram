25427797a7a3fcfa91ff5bac37932dd5aedfaa54
runnerlizard <smolev_sergey@mail.ru>
2021-12-16 16:55:16 +0300

    admin need fix

--- bot_telegram.py
+++ bot_telegram.py
@@ -6,8 +6,9 @@ from handlers import client, admin, other
 async def on_startup(_):
     print('bot online')
 
-client.register_handlers_client(dp)
-other.register_handlers_other(dp)
 
+client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
+other.register_handlers_other(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
